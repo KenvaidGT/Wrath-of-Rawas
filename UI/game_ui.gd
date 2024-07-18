7c2ec7ef8287226c5ce45d4d5ef4ea2a916a6fdb
unknown <medvedmaratus@gmail.com>
2024-07-18 13:57:25 +0300

    Game and wave UI

--- UI/game_ui.gd
+++ UI/game_ui.gd
@@ -2,11 +2,11 @@ extends Control
 
 @onready var game_ui: Control = $"."
 @onready var wave_ui: Control = $wave_UI
+var enemy = 0
 
 func _ready():
 	wave_ui.visible = false
 	var start_button = $M/VB/Start
-	start_button.pressed.connect(_on_start_pressed)
 
 func _process(delta):
 	pass
@@ -23,3 +23,13 @@ func _on_tower_2_pressed():
 
 func _on_tower_3_pressed():
 	print("Tower3_build")
+
+func _on_enemy_1_pressed():
+	enemy += 1
+	print(enemy)
+
+func _on_delet_tower_pressed():
+	print("Delet_tower")
+
+func _on_wall_pressed():
+	print("Wall")
