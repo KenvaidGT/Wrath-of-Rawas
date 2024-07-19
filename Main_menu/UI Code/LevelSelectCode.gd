10d38eda848f4bc5f10b94aeeeffc01ea81fc7d6
GMORDEX <gmordexai@gmail.com>
2024-07-19 18:11:48 +0300

    Made level selection page and connected them to 3 additional maps (Maps need to be made

--- Main_menu/UI Code/LevelSelectCode.gd
+++ Main_menu/UI Code/LevelSelectCode.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
 func _ready():
-	pass
-
-func _process(delta):
-	pass
-
-func on_start_pressed():
-	pass
+	$Level1.connect("pressed", Callable(self, "on_level1_pressed"))
+	$Level2.connect("pressed", Callable(self, "on_level2_pressed"))
+	$Level3.connect("pressed", Callable(self, "on_level3_pressed"))
+	$Level4.connect("pressed", Callable(self, "on_level4_pressed"))
 	
+func on_level1_pressed():
+	get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn").queue_free()
+	var game_scene = load("res://GameScenes/Map1.tscn").instantiate()
+	add_child(game_scene)
 
-func _on_exit_pressed():
-	get_tree().change_scene_to_file("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn")
+func on_level2_pressed():
+	get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn").queue_free()
+	var game_scene = load("res://GameScenes/Map2.tscn").instantiate()
+	add_child(game_scene)
+
+func on_level3_pressed():
+	get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn").queue_free()
+	var game_scene = load("res://GameScenes/Map3.tscn").instantiate()
+	add_child(game_scene)
+
+func on_level4_pressed():
+	get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn").queue_free()
+	var game_scene = load("res://GameScenes/Map4.tscn").instantiate()
+	add_child(game_scene)
+
