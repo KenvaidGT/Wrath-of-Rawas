06c415874ec5b766451e8586383edc78ec962f09
GMORDEX <gmordexai@gmail.com>
2024-07-22 00:38:57 +0300

    Simplified level selection code

--- Main_menu/UI Scenes/LevelSelect Visual/level_select.gd
+++ Main_menu/UI Scenes/LevelSelect Visual/level_select.gd
@@ -1,43 +1,24 @@
 extends Node2D
 
 func _ready():
-	$Level1.pressed.connect(self.on_level1_pressed)
-	$Level2.pressed.connect(self.on_level2_pressed)
-	$Level3.pressed.connect(self.on_level3_pressed)
-	$Level4.pressed.connect(self.on_level4_pressed)
-	$Exit.pressed.connect(self.on_exit_pressed)
-
+	$Level1.connect("pressed", Callable(self, "on_level1_pressed"))
+	$Level2.connect("pressed", Callable(self, "on_level2_pressed"))
+	$Level3.connect("pressed", Callable(self, "on_level3_pressed"))
+	$Level4.connect("pressed", Callable(self, "on_level4_pressed"))
+	$Exit.connect("pressed", Callable(self, "on_exit_pressed"))
+	
 func on_level1_pressed():
-	var level_select = get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn")
-	if level_select:
-		level_select.queue_free()
-	var game_scene = load("res://GameScenes/Map1.tscn").instantiate()
-	add_child(game_scene)
+	get_tree().change_scene_to_file("res://GameScenes/debug_lvl.tscn")
 
 func on_level2_pressed():
-	var level_select = get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn")
-	if level_select:
-		level_select.queue_free()
-	var game_scene = load("res://GameScenes/Map2.tscn").instantiate()
-	add_child(game_scene)
+	get_tree().change_scene_to_file("res://GameScenes/Map1.tscn")
 
 func on_level3_pressed():
-	var level_select = get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn")
-	if level_select:
-		level_select.queue_free()
-	var game_scene = load("res://GameScenes/Map3.tscn").instantiate()
-	add_child(game_scene)
+	get_tree().change_scene_to_file("res://GameScenes/Map2.tscn")
 
 func on_level4_pressed():
-	var level_select = get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn")
-	if level_select:
-		level_select.queue_free()
-	var game_scene = load("res://GameScenes/Map4.tscn").instantiate()
-	add_child(game_scene)
-
+	get_tree().change_scene_to_file("res://GameScenes/Map3.tscn")
+	
 func on_exit_pressed():
-	var level_select = get_node("res://Main_menu/UI Scenes/LevelSelect Visual/level_select.tscn")
-	if level_select:
-		level_select.queue_free()
-	var main_menu_scene = load("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn").instantiate()
-	get_tree().root.add_child(main_menu_scene)
+	get_tree().change_scene_to_file("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn")
+
