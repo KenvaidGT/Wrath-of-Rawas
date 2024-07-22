31ec5b175284179624f6ed01f1998b6a1f0ccb21
Бодя Рекс <bombi4es@gmail.com>
2024-07-22 13:58:49 +0300

    button exit start working

--- Main_menu/UI Code/ExitScene.gd
+++ Main_menu/UI Code/ExitScene.gd
@@ -6,4 +6,4 @@ func _ready():
 	exit_button.pressed.connect(on_exit_pressed)
 
 func on_exit_pressed():
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn")
--- Main_menu/UI Scenes/LevelSelect Visual/level_select.gd
+++ Main_menu/UI Scenes/LevelSelect Visual/level_select.gd
@@ -1,24 +1,26 @@
 extends Node2D
-
-func _ready():
-	$Level1.connect("pressed", Callable(self, "on_level1_pressed"))
-	$Level2.connect("pressed", Callable(self, "on_level2_pressed"))
-	$Level3.connect("pressed", Callable(self, "on_level3_pressed"))
-	$Level4.connect("pressed", Callable(self, "on_level4_pressed"))
-	$Exit.connect("pressed", Callable(self, "on_exit_pressed"))
 	
-func on_level1_pressed():
-	get_tree().change_scene_to_file("res://GameScenes/debug_lvl.tscn")
-
-func on_level2_pressed():
+	
+	
+func _on_level_1_pressed():
 	get_tree().change_scene_to_file("res://GameScenes/Map1.tscn")
-
-func on_level3_pressed():
+	
+	
+	
+func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://GameScenes/Map2.tscn")
-
-func on_level4_pressed():
+	
+	
+	
+func _on_level_3_pressed():
 	get_tree().change_scene_to_file("res://GameScenes/Map3.tscn")
 	
-func on_exit_pressed():
+	
+	
+func _on_debug_lvl_pressed():
+	get_tree().change_scene_to_file("res://GameScenes/debug_lvl.tscn")
+	
+	
+func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn")
-
+	
