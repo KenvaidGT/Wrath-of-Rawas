346acdc3d376b021ac1527577d52020bb7aea1a5
unknown <medvedmaratus@gmail.com>
2024-07-25 19:14:21 +0300

    Anamy path

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,12 +1,15 @@
-extends Node3D
+extends CharacterBody3D
 
-var Hp : int = 100
-var speed
-
-
-func _ready():
-	pass 
-
+@onready var nav_agent = $NavigationAgent3D
+var speed = 5.0
 
 func _process(delta):
-	pass
+	var next_location = nav_agent.get_next_path_position()
+	var current_location = global_transform.origin
+	var new_velocity = (next_location - current_location).normalized() * speed
+
+	velocity = velocity.move_toward(new_velocity, .25)
+	move_and_slide()
+
+func update_target_location(target_laoction):
+	nav_agent. target_position = target_laoction
--- GameScenes/Map1.gd
+++ GameScenes/Map1.gd
@@ -1,19 +1,21 @@
 extends Node3D
 
+@onready var tower = $Tower
+
 @onready var enemy_container: Node3D = $Enemy
 @onready var timer: Timer = $Enemies/Spawner
 var enemy_preload = preload("res://Enemies/Enemy.tscn")
 
+func _physics_process(_delta):
+	get_tree().call_group("Enemy" , "update_target_location" , tower.global_transform.origin)
+
 func _on_start_pressed():
-	if timer != null:
-		timer.start()
-	else:
-		print("Timer node not found!2")
+	timer.start()
 
 func _on_spawner_timeout():
 	spawn_enemy()
 
 func spawn_enemy():
 	var enemy = enemy_preload.instantiate()
-	enemy.position = Vector3(randi_range(-3.323, 5), 1, -6.616)
+	enemy.position = Vector3(randi_range(-3.323, 5), 3, -6.616)
 	$Enemies.add_child(enemy)
