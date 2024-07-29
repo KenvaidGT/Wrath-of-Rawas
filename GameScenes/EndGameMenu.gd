90a57c207083492ffc8334234039d39ca1c1db2a
KenvaidGT <KenvaidGT@gmail.com>
2024-07-29 14:51:57 +0300

    Deleted doesnt working GMORDEX towers

--- GameScenes/EndGameMenu.gd
+++ GameScenes/EndGameMenu.gd
@@ -1,11 +1,4 @@
 extends Control
 
-
-@onready var MainMenu_button = $"VBoxContainer/MainMenu button"
- 
-func _ready():
-	MainMenu_button.pressed.connect(_on_main_menu_button_pressed)
-
-
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn")
