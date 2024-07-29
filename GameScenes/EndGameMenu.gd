3d236fe5373f45e4c9a8a48bd02532676756fa82
KenvaidGT <KenvaidGT@gmail.com>
2024-07-29 12:14:55 +0300

    Fix win/lose menu bug button

--- GameScenes/EndGameMenu.gd
+++ GameScenes/EndGameMenu.gd
@@ -8,4 +8,4 @@ func _ready():
 
 
 func _on_main_menu_button_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://Main_menu/UI Scenes/MainMenu Visual/MainScene.tscn")
--- UI/wave_ui.gd
+++ UI/wave_ui.gd
@@ -11,7 +11,6 @@ var Mana = 10
 func _ready():
 	lose_ui.visible = false
 	win_ui.visible = false
-	MainMenu_button.pressed.connect(_on_main_menu_button_pressed)
 	
 func _on_attack_pressed():
 	print("Hp -1")
@@ -36,5 +35,3 @@ func _on_lose_pressed():
 func _on_win_pressed():
 	win_ui.visible = true
 
-func _on_main_menu_button_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
