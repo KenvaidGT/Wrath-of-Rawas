570edb5e7e1683493ea2f8f0a381c42125dc1a90
Faysi505 <505.faysi@gmail.com>
2024-08-10 19:41:24 +0300

    Tiny level 2 UI improvements

--- GameScenes/ScriptManager.gd
+++ GameScenes/ScriptManager.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
-@export var wall_scene = "res://Towers_and_wall/Wall.tscn"  
+@export var wall_scene: PackedScene
 
-var current_wall: StaticBody3D = null
-var is_placing_wall = false
-var grid_size = 2.0  # Размер сетки для привязки
-
+func _ready():
+	# Example of dynamically adding a wall
+	var wall_instance = wall_scene.instantiate()
+	wall_instance.transform.origin = Vector3(0, 1, 0) # Position the wall
+	add_child(wall_instance)
