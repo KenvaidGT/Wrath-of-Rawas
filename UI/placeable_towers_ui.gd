4e773a1a4f1e07c59470c36139160385d233644b
Faysi505 <505.faysi@gmail.com>
2024-08-12 14:36:04 +0300

    Added raw code to wall placement

--- UI/placeable_towers_ui.gd
+++ UI/placeable_towers_ui.gd
@@ -1,17 +1,35 @@
 extends Control
 
+@export var wall_activity_button_icon:Texture2D
+@export var wall_activity_draggable:PackedScene
 
-func _process(delta):
-	pass
-	
-#Wall button
+var cam:Camera3D
+var _is_dragging:bool = false
+var _draggable:Node
+var _is_valid_location:bool = false
+var _last_valid_location:Vector3
+var RAYCAST_LENGTH:int = 100
+
 func _on_wall_button_button_down():
-	print("Button down")
+	_is_dragging = true
+	_draggable = wall_activity_draggable.instantiate()
+	add_child(_draggable)
+	
 func _on_wall_button_button_up():
-	print("Button up")
-
-#Tower1 button
-func _on_tower_1_button_button_down():
-	print("Button down")
-func _on_tower_1_button_button_up():
-	print("Button up")
+	_is_dragging = false
+	
+func _physics_process(_delta):
+	if _is_dragging:
+		
+		var space_state = _draggable.get_world_3d().direct_space_state
+		var mouse_pos:Vector2 = get_viewport().get_mouse_position()
+		var origin:Vector3 = cam.project_ray_origin(mouse_pos)
+		var end:Vector3 = origin + cam.project_ray_normal(mouse_pos) * RAYCAST_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+		var rayResult:Dictionary = space_state.intersect_ray(query)
+		if rayResult.size() > 0:
+			var co:CollisionObject3D = rayResult.get("collider")
+		else:
+			_draggable.visible = false
+			_is_valid_location = false
